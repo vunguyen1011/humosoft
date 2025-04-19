d40f66a0db885e852279d03e290109e0f2e0defe
vunguyen1011 <vunguyen10112k4@gmail.com>
2025-04-20 00:23:39 +0700

    fix mail

--- src/main/java/com/example/Humosoft/Controller/MailController.java
+++ src/main/java/com/example/Humosoft/Controller/MailController.java
@@ -1,6 +1,7 @@
 package com.example.Humosoft.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("mails")
 public class MailController {
 	private final MailService mailService;
-	@GetMapping("/sendrequest")
+	@PostMapping("/sendrequest")
 	public Apiresponse<Void> sendRequestForgotPassword(@RequestBody ForgotPassword request) throws MessagingException{
 		mailService.sendRequestForgotPassword(request.getToEmail(),request.getLink());
 		return Apiresponse.<Void>builder()
